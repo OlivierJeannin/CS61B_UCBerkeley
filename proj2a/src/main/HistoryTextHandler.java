e49c912846f0f2fa59d4b23b7b3f0b60d46830cc
Olivier <2150137096@qq.com>
2025-10-30 19:36:14 +0800

    proj2a: HistoryTextHandler

---
+++ proj2a/src/main/HistoryTextHandler.java
@@ -0,0 +1,29 @@
+package main;
+
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+
+import java.util.List;
+
+public class HistoryTextHandler extends NgordnetQueryHandler {
+    private final NGramMap map;
+
+    public HistoryTextHandler(NGramMap map) {
+        this.map = map;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        StringBuilder response = new StringBuilder();
+        for (String word: words) {
+            String wordString = word + ": " + map.weightHistory(word, startYear, endYear).toString() + "\n";
+            response.append(wordString);
+        }
+        return response.toString();
+    }
+}
